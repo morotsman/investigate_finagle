4bd64add2005340e5f5198164e4183fc7c7f24d1
Niklas Leopold <niklas.leopold@verisure.com>
2021-01-03 16:38:06 +0100

    finagle service

--- src/main/scala/com/github/morotsman/investigate_finagle_service/SimpleHttpService.scala
+++ src/main/scala/com/github/morotsman/investigate_finagle_service/SimpleHttpService.scala
@@ -8,14 +8,14 @@ object SimpleHttpService extends App {
   val service = new Service[http.Request, http.Response] {
     def apply(req: http.Request): Future[http.Response] = {
       println(s"Received request: $req")
-      Future.value(
-        http.Response(req.version, http.Status.Ok)
-      )
+      Future.value{
+        val response = http.Response(req.version, http.Status.Ok)
+        response.contentString = "Hello world!"
+        response
+      }
     }
   }
 
   val server = Http.serve(":8080", service)
   Await.ready(server)
-
-
 }
